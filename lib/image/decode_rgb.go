ea69671b75f3f24da652fe36f4e3255dd471f5ea
Markus Tzoe <chou.marcus@gmail.com>
2021-02-26 23:39:02 +0800

    Fixes decodeARGB

--- lib/image/decode_rgb.go
+++ lib/image/decode_rgb.go
@@ -8,6 +8,9 @@ func decodeARGB(frame []byte, width, height int) (image.Image, error) {
 		return nil, ErrInsufficientFrameBuffer
 	}
 	r := image.Rect(0, 0, width, height)
+	for i := 0; i < size; i += 4 {
+		frame[i], frame[i+2] = frame[i+2], frame[i]
+	}
 	return &image.RGBA{
 		Pix:    frame[:size:size],
 		Stride: 4 * r.Dx(),
